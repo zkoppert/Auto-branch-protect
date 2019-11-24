c1218bac59bada8ea9df8640d19a9f40a4c4bdb6
Zack Koppert <zacheryk89@gmail.com>
2019-11-23 21:37:22 -0800

    Add functional issue as a notification
    Signed-off-by: Zack Koppert <zacheryk89@gmail.com>

--- app.py
+++ app.py
@@ -29,12 +29,29 @@ def webhook():
             }
 			session = requests.session()
 			session.auth = (user, cred)
-			response = session.put(payload['repository']['url'] + '/branches/master/protection', json.dumps(branch_protection))
-			if response.status_code == 200:
-				print('Branch protection created successfully. Status code: ', response.status_code)
+			response_1 = session.put(payload['repository']['url'] + '/branches/master/protection', json.dumps(branch_protection))
+			if response_1.status_code == 200:
+				print('Branch protection created successfully. Status code: ', response_1.status_code)
+
+				# Create issue in repo notifying user of branch protection
+				try:
+					if payload['repository']['has_issues']:
+						issue = {"title": "New Protection Added", "body": "@" + user + " A new branch protection was added to the master branch." }
+						session = requests.session()
+						session.auth = (user, cred)
+						response_2 = session.post(payload['repository']['url'] + '/issues', json.dumps(issue))
+						if response_2.status_code == 201:
+						    print('Issue created successfully. Status code: ', response_2.status_code)
+						else:
+				   			print('Unable to create issue. Status code: ',  response_2.status_code)
+					else:
+						print('This repo has no issues so one cannot be created at this time.')
+				except KeyError:
+					# Request did not contain information about if the repository has issues enabled
+					pass
 			else:
-				print(response.content)
-				print('Unable to create branch protection. Status code: ',  response.status_code)
+				print(response_1.content)
+				print('Unable to create branch protection. Status code: ',  response_1.status_code)
 	except KeyError:
 		# Ignore POST payload since it is not a create action
 		pass
